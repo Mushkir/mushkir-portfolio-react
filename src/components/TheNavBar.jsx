916314de467b86f9aa79bbb0ecfc41d826097a90
Mohamed Mushkir <mushkirmohamed@gmail.com>
2024-03-10 22:53:47 +0530

    config: components and other stuffs initialized

---
+++ src/components/TheNavBar.jsx
@@ -0,0 +1,4 @@
+const TheNavBar = () => {
+  return <h1>NavBar</h1>;
+};
+export default TheNavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./assets/css/index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import TheGuestLayout from "./layouts/TheGuest.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TheGuestLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
