6bb950805783025ae46f8d3d3c7f448f78c957ec
Mohamed Mushkir <mushkirmohamed@gmail.com>
2024-03-14 22:41:46 +0530

    style: homepage works card created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,20 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Card = ({ heading, icon, navigate }) => {
+const Card = ({ heading, icon }) => {
   //
   return (
-    <Link
-      to={navigate}
-      className="bg-primary-blue text-main-heading-color flex flex-col justify-center items-center p-5 rounded-md sm:w-[50%] sm:h-[200px] mb-5 hover:scale-[1.05] hover:transition 500 hover:bg-[#081426]"
-    >
+    <div className="bg-primary-blue text-main-heading-color flex flex-col justify-center items-center p-5 rounded-md sm:w-[50%] sm:h-[200px] mb-5 hover:scale-[1.05] hover:transition 500 hover:bg-[#081426]">
       {/* Icon */}
       <div>{icon}</div>
       <h5 className="sm:text-xl text-center mt-3 sm:mt-5">{heading}</h5>
-    </Link>
+    </div>
   );
 };
 
 Card.propTypes = {
   heading: PropTypes.string,
   icon: PropTypes.element,
-  navigate: PropTypes.string,
 };
 
 export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,13 @@ import TechStacks from "../components/TechStacks";
 import ServicesInfo from "../components/ServicesDetail";
 import TheFooterSection from "../components/Footer";
 import TheFootNote from "../components/TheFootNote";
-import taskList from "../data/taskList.json";
+// import taskList from "../data/taskList.json";
 import TheProjectSection from "../components/TheProjectSection";
 
 // import { Tooltip } from "react-tooltip";
 
 const HomePage = () => {
-  console.log(taskList[0].tasks.frontend);
+  // console.log(taskList[0].tasks.frontend);
   return (
     <div>
       <HeroDetail />
