76625f38388e0a206c86f4e52d91d47cd1e6b47d
Mushkir <mushkirmohamed@gmail.com>
2025-10-31 01:54:51 +0530

    feat: added whatsapp floating button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import TheFooterSection from "../components/Footer";
 import TheFootNote from "../components/TheFootNote";
 import TheProjectSection from "../components/TheProjectSection";
 import TheHeaderProgressBar from "../components/HeaderProgressBar/TheHeaderProgressBar";
+import { FloatingWhatsApp } from "@dxkit-org/react-floating-whatsapp";
 
 const HomePage = () => {
   return (
@@ -27,6 +28,19 @@ const HomePage = () => {
       <TheFooterSection />
 
       <TheFootNote />
+
+      <FloatingWhatsApp
+        phoneNumber="+94777195282"
+        accountName="Mushkir"
+        avatar={`${`https://github.com/Mushkir`}.png`}
+        statusMessage="Typically replies within 1 hour"
+        chatMessage="Hello! 👋 How can we help you today?"
+        darkMode={true}
+        allowClickAway={true}
+        allowEsc={true}
+        notification={true}
+        notificationSound={true}
+      />
     </div>
   );
 };
